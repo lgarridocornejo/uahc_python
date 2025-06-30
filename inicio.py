fd4f4758aba9e4321b020666902bfd80fdf7f79d
Luis Garrido <lgarrido.cornejo@gmail.com>
2025-06-30 07:02:49 -0400

    20250631 Update inicio.py

--- inicio.py
+++ inicio.py
@@ -20,9 +20,9 @@ codigos_validos = {
     "0b9cf1ec": "pruebas/proyecto_0b9cf1ec.ipynb",
     "acd71a71": "pruebas/proyecto_acd71a71.ipynb",
     "f4f9bf9f": "pruebas/proyecto_f4f9bf9f.ipynb",
-    "6b558034": "pruebas/proyecto_6b558034.ipynb",
-    "0d5dfc8d": "pruebas/proyecto_0d5dfc8d.ipynb",
-    "1bbad4bf": "pruebas/proyecto_1bbad4bf.ipynb"
+    "6b558034": "pruebas/proyecto_aaaaaaaa.ipynb",
+    "0d5dfc8d": "pruebas/proyecto_bbbbbbbb.ipynb",
+    "1bbad4bf": "pruebas/proyecto_cccccccc.ipynb"
 }
 
 # Crear archivo de registro si no existe
